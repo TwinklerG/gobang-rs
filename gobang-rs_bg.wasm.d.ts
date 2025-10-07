9d21da9cd3f2001cc41dc42f2f70354935b2acc8
TwinklerG <TwinklerG@users.noreply.github.com>
2025-10-07 09:20:37 +0000

    Deploy WebAssembly build to GitHub Pages 7fba4da0b70ecaa61d0939d3b66a82414b2645d6

---
+++ gobang-rs_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure252_externref_shim: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h8f56fc2e916db84b_multivalue_shim: (a: number, b: number) => [number, number];
+export const __externref_table_dealloc: (a: number) => void;
+export const closure149_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
